8b00837bff5c6b5190ae5e6eca459c20b48d00e7
Billy Onjea <vasili.onjea@gmail.com>
2022-11-25 16:41:23 +0200

    comment

--- testing/utils.js
+++ testing/utils.js
@@ -1,6 +1,7 @@
 export function noop() {}
 export async function asyncNoop() {}
 
+// https://nodejs.org/en/docs/guides/event-loop-timers-and-nexttick/#why-would-that-be-allowed
 export function flushPromises() {
   return new Promise(process.nextTick);
 }
